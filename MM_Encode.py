7ce91ea979833dacb02e7f09d46337c9db68fc76
Gugustinette <mercier.augustin@outlook.fr>
2021-01-05 18:45:45 +0100

    Deleted Binary Wall + Created Encrypt/Decrypt Function

--- MM_Encode.py
+++ MM_Encode.py
@@ -97,232 +97,208 @@ More :
 # Takes the input message to encode
 message = str(input("Give a message to encode"))
 
-# Init the list
-caraList = list(message)
+def Encrypt(message) :
 
-key = ''
+    # Init the list
+    caraList = list(message)
 
-a_value = 1
-b_value = 0
-c_value = 0
-e_value = 0
+    key = ''
 
-max_char_binary = 0
+    a_value = ''
+    b_value = ''
+    b_len = ''
+    d_value = ''
+    d_len = ''
 
-def BinaryToString(binary):  
-         
-    binary1 = binary  
-    decimal, i, n = 0, 0, 0
-    while(binary != 0):  
-        dec = binary % 10
-        decimal = decimal + dec * pow(2, i)  
-        binary = binary//10
-        i += 1
-    str_decimal = chr(decimal)
-    return (str_decimal)
-
-def ToMaxChar(str_nb, nb_char) : # Add one or many zero before a number to make it longer (return the number as a string)
-    x = nb_char - len(str_nb)
-    for i in range(x) :
-        str_nb = str('0' + str_nb)
-    return str_nb
-
-def Even(nb) : # Return true if even, else false
-    if nb % 2 == 0 :
-        return True
-    else :
-        return False
-
-def CutElements(list_a) : # Divide elements of many character into many elements of one character (takes and return a list)
-    x = 0
-    y = 0
-    g = 0
-    for i in range(len(list_a)) :
-        g = 0
-        if len(list_a[x]) > 1 :
-            aLet = list_a[x]
-            list_a.pop(x)
-            y = len(aLet) - 1
-            for n in range(len(aLet)) :
-                list_a.insert(x, aLet[y])
-                y -= 1
-                g += 1
-            x += g - 1
-        x += 1
-    return list_a
-
-def Shift(str_input, value) : # Shift all characters of a string by a certain value (return a string)
-    y = value
-    fLet = ''
-    for i in range(value) :
-        fLet = str(fLet + str_input[len(str_input) - y])
-        y -= 1
-    for i in range(len(str_input) - value) :
-        fLet = str(fLet + str_input[i])
-    return fLet
-
-def Seperate(str_input, nbElement) : # Seperate a string into x element (of equal number of character) in a list (return a list)
-    list_s = []
-    nbChar = len(str_input) // nbElement
-    kLet = ''
-    x = 0
-    for i in range(nbElement) :
-        for i in range(nbChar) :
-            kLet = str(kLet + str_input[x])
-            x += 1
-        list_s.append(kLet)
-        kLet = ''
-    return list_s
-
-def wallA(list_a) : # -9 to 9 Ascii Ceasar Method (0 not included)
-    global a_value
-    a_value = random.randint(-9, 9)
-    while a_value == 0 :
-        a_value = random.randint(-9, 9)
-    for i in range(len(list_a)):
-        kLet = ord(list_a[i])
-        kLet += a_value
-        list_a[i] = chr(kLet)
-    list_a = CutElements(list_a)
-    return list_a
-
-def wallB(list_a) : # Hex Encryption Shift
-    global b_value
-    nbElement = len(list_a)
-    x = 0
-    for i in range(len(list_a)): # Convert all element to hex
-        kLet = hex(ord(list_a[x])).replace('0x', '')
-        list_a[x] = kLet
-        x += 1
-    fLet = ''
-    for i in range(len(list_a)) : # Make it one big string
-        fLet = str(fLet + list_a[i])
-    list_a = []
-    b_value = random.randint(1, len(fLet) - 1)
-    fLet = Shift(fLet, b_value) # Shift
-    list_a = Seperate(fLet, nbElement) # Seperate back into many elements of a list
-    list_a = CutElements(list_a) # Cut every elements of many character into many elements of one character
-    return list_a
-
-def wallC(list_a) : # Binary Shift
-    global c_value, max_char_binary
-    nbElement = len(list_a)
     max_char_binary = 0
-    for i in range(len(list_a)) : # Binary Conversion of list elements
-        kLet = bin(ord(list_a[i])).replace('0b', '')
-        if len(kLet) > max_char_binary : # Register longest number of character for one binary
-            max_char_binary = len(kLet)
-        list_a[i] = kLet
-    for i in range(len(list_a)) : # Make all binaries the same lenght
-        if len(list_a[i]) < max_char_binary :
-            list_a[i] = ToMaxChar(list_a[i], max_char_binary)
-    rLet = ''
-    for i in range(len(list_a)) : # Fusion of all element into one big binary string one
-        rLet = str(rLet + list_a[i])
-    list_a = []
-    c_value = random.randint(1, len(rLet) - 1)
-    fLet = Shift(rLet, c_value) # Shift characters
-    list_a = Seperate(fLet, nbElement) # Seperate back character into elements of a string
-    for i in range(len(list_a)) : # Convert every binary elements to string
-        kLet = list_a[i]
-        kLet = BinaryToString(int(kLet))
-        list_a[i] = kLet
-    return list_a
 
-def wallD(list_a) : # Re-Order Method
-    if Even(len(list_a)) :
-        d_value = len(list_a) // 2
-    else : # Odd
-        d_value = (len(list_a) - 1) // 2
-    x = 0
-    for i in range(d_value) :
-        stock = list_a[x]
-        list_a[x] = list_a[x + 1]
-        list_a[x + 1] = stock
-        x += 2
-    return list_a
+    def BinaryToString(binary):  
+            
+        binary1 = binary  
+        decimal, i, n = 0, 0, 0
+        while(binary != 0):  
+            dec = binary % 10
+            decimal = decimal + dec * pow(2, i)  
+            binary = binary//10
+            i += 1
+        str_decimal = chr(decimal)
+        return (str_decimal)
 
-def wallE(list_a) : #Octal Encryption Shift
-    global e_value
-    nbElement = len(list_a)
-    for i in range(len(list_a)) : # Convert all element to oct
-        kLet = oct(ord(list_a[i])).replace('0o', '')
-        if len(kLet) < 3 :
-            kLet = ToMaxChar(kLet, 3)
-        list_a[i] = kLet
-    fLet = ''
-    for i in range(len(list_a)) : # Make it one big string
-        fLet = str(fLet + list_a[i])
-    list_a = []
-    e_value = random.randint(1, len(fLet) - 1)
-    fLet = Shift(fLet, e_value) # Shift
-    list_a = Seperate(fLet, nbElement) # Seperate back into many elements of a list
-    list_a = CutElements(list_a) # Cut every elements of many character into many elements of one character
-    return list_a
+    def ToMaxChar(str_nb, nb_char) : # Add one or many zero before a number to make it longer (return the number as a string)
+        x = nb_char - len(str_nb)
+        for i in range(x) :
+            str_nb = str('0' + str_nb)
+        return str_nb
 
-def callEncrypt(list_a, str_input) : # Apply wall depending of the string (A, B, C, ...)
-    if str_input == 'A' :
-        list_a = wallA(list_a)
-    else :
-        if str_input == 'B' :
-            list_a = wallB(list_a)
+    def Even(nb) : # Return true if even, else false
+        if nb % 2 == 0 :
+            return True
         else :
-            if str_input == 'C' :
-                list_a = wallC(list_a)
+            return False
+
+    def CutElements(list_a) : # Divide elements of many character into many elements of one character (takes and return a list)
+        x = 0
+        y = 0
+        g = 0
+        for i in range(len(list_a)) :
+            g = 0
+            if len(list_a[x]) > 1 :
+                aLet = list_a[x]
+                list_a.pop(x)
+                y = len(aLet) - 1
+                for n in range(len(aLet)) :
+                    list_a.insert(x, aLet[y])
+                    y -= 1
+                    g += 1
+                x += g - 1
+            x += 1
+        return list_a
+
+    def Shift(str_input, value) : # Shift all characters of a string by a certain value (return a string)
+        y = value
+        fLet = ''
+        for i in range(value) :
+            fLet = str(fLet + str_input[len(str_input) - y])
+            y -= 1
+        for i in range(len(str_input) - value) :
+            fLet = str(fLet + str_input[i])
+        return fLet
+
+    def Seperate(str_input, nbElement) : # Seperate a string into x element (of equal number of character) in a list (return a list)
+        list_s = []
+        nbChar = len(str_input) // nbElement
+        kLet = ''
+        x = 0
+        for i in range(nbElement) :
+            for i in range(nbChar) :
+                kLet = str(kLet + str_input[x])
+                x += 1
+            list_s.append(kLet)
+            kLet = ''
+        return list_s
+
+    def wallA(list_a) : # -9 to 9 Ascii Ceasar Method (0 not included)
+        a_value = random.randint(-9, 9)
+        while a_value == 0 :
+            a_value = random.randint(-9, 9)
+        for i in range(len(list_a)):
+            kLet = ord(list_a[i])
+            kLet += a_value
+            list_a[i] = chr(kLet)
+        list_a = CutElements(list_a)
+        return list_a, str(a_value)
+
+    def wallB(list_a) : # Hex Encryption Shift
+        nbElement = len(list_a)
+        x = 0
+        for i in range(len(list_a)): # Convert all element to hex
+            kLet = hex(ord(list_a[x])).replace('0x', '')
+            list_a[x] = kLet
+            x += 1
+        fLet = ''
+        for i in range(len(list_a)) : # Make it one big string
+            fLet = str(fLet + list_a[i])
+        list_a = []
+        b_value = random.randint(1, len(fLet) - 1)
+        fLet = Shift(fLet, b_value) # Shift
+        list_a = Seperate(fLet, nbElement) # Seperate back into many elements of a list
+        list_a = CutElements(list_a) # Cut every elements of many character into many elements of one character
+        return list_a, str(b_value)
+
+    def wallC(list_a) : # Re-Order Method
+        if Even(len(list_a)) :
+            d_range = len(list_a) // 2
+        else : # Odd
+            d_range = (len(list_a) - 1) // 2
+        x = 0
+        for i in range(d_range) :
+            stock = list_a[x]
+            list_a[x] = list_a[x + 1]
+            list_a[x + 1] = stock
+            x += 2
+        d_value = 1
+        return list_a, str(d_value)
+
+    def wallD(list_a) : #Octal Encryption Shift
+        nbElement = len(list_a)
+        for i in range(len(list_a)) : # Convert all element to oct
+            kLet = oct(ord(list_a[i])).replace('0o', '')
+            if len(kLet) < 3 :
+                kLet = ToMaxChar(kLet, 3)
+            list_a[i] = kLet
+        fLet = ''
+        for i in range(len(list_a)) : # Make it one big string
+            fLet = str(fLet + list_a[i])
+        list_a = []
+        e_value = random.randint(1, len(fLet) - 1)
+        fLet = Shift(fLet, e_value) # Shift
+        list_a = Seperate(fLet, nbElement) # Seperate back into many elements of a list
+        list_a = CutElements(list_a) # Cut every elements of many character into many elements of one character
+        return list_a, str(e_value)
+
+    def callEncrypt(list_a, str_input) : # Apply wall depending of the string (A, B, C, ...)
+        if str_input == 'A' :
+            list_a, key_value = wallA(list_a)
+            wall_applied = 'A'
+        else :
+            if str_input == 'B' :
+                list_a, key_value = wallB(list_a)
+                wall_applied = 'B'
             else :
-                if str_input == 'D' :
-                    list_a = wallD(list_a)
+                if str_input == 'C' :
+                    list_a, key_value = wallC(list_a)
+                    wall_applied = 'C'
                 else :
-                    list_a = wallE(list_a)
-    return list_a
+                    list_a, key_value = wallD(list_a)
+                    wall_applied = 'D'
+        return list_a, wall_applied, key_value
 
-list_wall = ['A', 'B', 'C', 'D', 'E']
-list_order = []
+    list_wall = ['A', 'B', 'C', 'D']
+    list_order = []
 
-for i in range(5) : # Initialize the order of the wall
-    x = random.randint(0, len(list_wall) - 1)
-    kLet = list_wall[x]
-    list_wall.pop(x)
-    list_order.append(kLet)
+    for i in range(len(list_wall)) : # Initialize the order of the wall
+        x = random.randint(0, len(list_wall) - 1)
+        kLet = list_wall[x]
+        list_wall.pop(x)
+        list_order.append(kLet)
 
-for i in range(5) : # Call the wall in order
-    caraList = callEncrypt(caraList, list_order[i])
+    wall_applied = ''
+    key_value = 0
 
-# Initialize all len values
-bx = len(str(b_value))
-cx = len(str(c_value))
-ex = len(str(e_value))
+    for i in range(len(list_order)) : # Call the wall in order
+        caraList, wall_applied, key_value = callEncrypt(caraList, list_order[i])
+        if wall_applied == 'A' :
+            a_value = key_value
+        else :
+            if wall_applied == 'B' :
+                b_value = key_value
+                b_len = len(b_value)
+            else :
+                if wall_applied == 'D' :
+                    d_value = key_value
+                    d_len = len(d_value)
 
-# Add wall order to the key
-for i in range(len(list_order)) :
-    key = str(key + list_order[i])
+    # Add wall order to the key
+    for i in range(len(list_order)) :
+        key = str(key + list_order[i])
 
-# Add wall values and len values to the key
-key = str(key + str(a_value))
-key = str(key + str(bx))
-key = str(key + str(b_value))
-key = str(key + str(cx))
-key = str(key + str(c_value))
-key = str(key + str(max_char_binary))
-key = str(key + str(ex))
-key = str(key + str(e_value))
+    # Add wall values and len values to the key
+    key = str(key + a_value)
+    key = str(key + str(b_len))
+    key = str(key + b_value)
+    key = str(key + str(d_len))
+    key = str(key + d_value)
 
-pMes = ''
-for i in range(len(caraList)) :
-    pMes = str(pMes + caraList[i])
+    pMes = ''
+    for i in range(len(caraList)) :
+        pMes = str(pMes + caraList[i])
 
-print("Encrypted list is : ")
-print(caraList)
+    return pMes, key
+
+encrypted_message, key = Encrypt(message)
 
 print("Encrypted message is : ")
-print(pMes)
+print(encrypted_message)
 
 print("Key is : ")
 print(key)
-
-file1 = open("encrypt.txt", "a")
-
-file1.write(pMes)
-file1.write('\n' + key)
-
-file1.close()
